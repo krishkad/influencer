20b82eedf6576d8b3e8bf2c9018aaf965e9dc341
krishkad <krishkadam88@gmail.com>
2024-03-23 16:14:55 +0530

    repare

--- src/app/(tool)/tool/[tool]/page.tsx
+++ src/app/(tool)/tool/[tool]/page.tsx
@@ -1,11 +1,6 @@
-import Check from '@/components/shared/Check'
 import { Dashboard } from '@/components/shared/Dashboard'
-import ToolMobileNavbar from '@/components/shared/Tool.mobile.navbar'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import { ToolNavbar } from '@/constants/index.c'
 import React from 'react'
-import { Bell, Settings, Star } from 'react-feather'
 
 const Page = ({ params }: { params: { tool: string } }) => {
     const content: { Label: string, href: string, description: string, miniDescription: string } | any = ToolNavbar.find((item) => item.href.includes(params.tool))
